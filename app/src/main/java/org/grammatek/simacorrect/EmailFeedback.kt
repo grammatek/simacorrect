6ba1fa29ba16c44a08861648ad01a360233cfa3e
Kristober <kriztone1@gmail.com>
2022-04-28 11:29:45 +0000

    Adding new activites

---
+++ app/src/main/java/org/grammatek/simacorrect/EmailFeedback.kt
@@ -0,0 +1,44 @@
+package org.grammatek.simacorrect
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class EmailFeedback : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sendEmail()
+    }
+
+    private fun sendEmail() {
+        Log.v(TAG, "Send email")
+        val recipientEmail = "info@grammatek.com"
+        val subject = getString(R.string.email_subject)
+        val msg = getString(R.string.email_message)
+        val emailIntent = Intent(Intent.ACTION_SENDTO)
+        emailIntent.data = Uri.parse("mailto:$recipientEmail")
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipientEmail))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        emailIntent.putExtra(Intent.EXTRA_TEXT, msg)
+        val sendMsg = getString(R.string.send_message)
+        try {
+            startActivity(Intent.createChooser(emailIntent, sendMsg))
+            finish()
+            Log.i(TAG, "Finished sending email...")
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                this@EmailFeedback,
+                "There is no email client installed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    companion object {
+        private val TAG = EmailFeedback::class.java.simpleName
+    }
+}
